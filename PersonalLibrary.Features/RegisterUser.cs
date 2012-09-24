dce78dc836a74dfc7ff33dc5534d10d8c0d988ff
Jeffrey Ng <jeffrey.s.h.ng@gmail.com>
2012-09-24 14:22:45 +0100

    Second scenario added
    Added second scenario. These are taken from the tutorial that I did last
    week.

--- PersonalLibrary.Features/RegisterUser.cs
+++ PersonalLibrary.Features/RegisterUser.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using PersonalLibrary_BDD.Controllers;
+using PersonalLibrary_BDD.Models;
 using TechTalk.SpecFlow;
 using System.Web.Mvc;
-
 
 namespace PersonalLibrary.Features
 {
@@ -15,6 +16,7 @@ namespace PersonalLibrary.Features
     {
         AccountController _controller;
         ActionResult _result;
+        RegisterModel _registerModel;
 
        [When(@"the user goes to the register user screen")]
         public void WhenTheUserGoesToTheRegisterUserScreen()
@@ -32,5 +34,33 @@ namespace PersonalLibrary.Features
                    _controller.ViewData["Title"],
                    "Page title is wrong");
         }
+
+        [Given(@"The user has entered all the information")]
+        public void GivenTheUserHasEnteredAllTheInformation()
+        {
+            _registerModel = new RegisterModel
+            {
+                UserName = "user" + new Random(1000).NextDouble().ToString(CultureInfo.InvariantCulture),
+                Email = "test@dummy.com",
+                Password = "test123",
+                ConfirmPassword = "test123"
+            };
+            _controller = new AccountController();
+        }
+
+        [When(@"He Clicks on Register button")]
+        public void WhenHeClicksOnRegisterButton()
+        {
+            _result = _controller.Register(_registerModel);
+        }
+
+        [Then(@"He should be redirected to the home page")]
+        public void ThenHeShouldBeRedirectedToTheHomePage()
+        {
+            const string expected = "Index";
+            Assert.IsNotNull(_result);
+            Assert.IsInstanceOf<ViewResult>(_result);
+        }
+
     }
 }
--- PersonalLibrary.Features/RegisterUser.feature
+++ PersonalLibrary.Features/RegisterUser.feature
@@ -7,3 +7,8 @@
 Scenario: Navigate to the Register Page
 	When the user goes to the register user screen
 	Then the register user view should be displayed
+
+Scenario: On Successful registration the user should be redirected to Home Page
+    Given The user has entered all the information
+    When He Clicks on Register button
+    Then He should be redirected to the home page
