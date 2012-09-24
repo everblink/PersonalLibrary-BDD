4ffd1e117502eb9a513626f88fb2dce93f7b4b1c
Jeffrey Ng <jeffrey.s.h.ng@gmail.com>
2012-09-24 15:14:48 +0100

    Added second feature, add item
    Needs extending but done enough to pass the tests. Needs refactoring.

---
+++ PersonalLibrary.Features/AddItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using PersonalLibrary_BDD.Controllers;
+using PersonalLibrary_BDD.Models;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace PersonalLibrary.Features
+{
+    [Binding]
+    class AddItem
+    {
+        AccountController _controller;
+        LoginModel _loginModel;
+        ActionResult _result;
+        AddItemController _addItemController;
+
+        [Given(@"the user is logged in")]
+        public void GivenTheUserIsLoggedIn()
+        {
+            _loginModel = new LoginModel
+            {
+                UserName = "Jeff",
+                Password = "password",
+                RememberMe = true
+            };
+            _controller = new AccountController();
+        }
+
+        [Given(@"they have clicked on Add Items link")]
+        public void GivenTheyHaveClickedOnAddItemsLink()
+        {
+            _addItemController = new AddItemController();
+            _result = _addItemController.Index();
+        }
+
+        [Then(@"the user should be redirected to the Add Items page")]
+        public void ThenTheUserShouldBeRedirectedToTheAddItemsPage()
+        {
+            const string expected = "Index";
+            Assert.IsNotNull(_result);
+            Assert.IsInstanceOf<ViewResult>(_result);
+        }
+
+    }
+}
---
+++ PersonalLibrary.Features/AddingItem.feature
@@ -0,0 +1,11 @@
+﻿Feature: AddingItem
+	In order to have a libary application
+    We need to have the ability to add items
+    For logged in users
+
+@mytag
+Scenario: User must be logged in to navigate to Add items screen
+	Given the user is logged in
+	And they have clicked on Add Items link
+	Then the user should be redirected to the Add Items page
+
